b68f7e5ae96d5a77ab411a3391b13d1f8b5868aa
TheLazySquid <TheLazySquid2@gmail.com>
2023-03-31 10:00:24 -0400

    Multiple scripts together

--- README.md
+++ README.md
@@ -10,18 +10,40 @@ Any gamemode specific script will pull up a hud to manage the script, which can 
 
 ### [General](/general.js)
 
-This script works on all gamemodes. After answering a question, it automatically saves it, and will highlight / move the correct answer once it comes up again. Do be wary, as answering too fast may cause you to be kicked. This script is disabled entirely by hitting shift three times.
+To run the general script, a local override must be set up. See below for instructions on various browsers. If you are using a browser that doesn't support local overrides, you can use the [v1 version of this script](/v1/general.js), which requires you answering all questions once before it works.
 
-![Highlighted Answer](/images/correct_example.png)
+Click [here](#setting-up-an-override) to see how to set up a local override.
+
+Once run, it automatically answers questions every ~1 second.
 
 ### [Classic](/gamemodes/classic.js)
 
 This should not be used in conjunction with the general script.
 
-The trick for classic is loosely based around a trick found in [non-reai's Gimkit hack](https://github.com/non-reai/Gimkit-Hacks) to answer questions, rather than manually storing and answering questions. It automatically answers questions and purchases upgrades, allowing you to sit back and relax.
+The trick for classic is loosely based around a trick found in [non-reai's Gimkit hack](https://github.com/non-reai/Gimkit-Hacks) to answer questions, rather than manually storing and answering questions. It automatically answers questions and purchases upgrades, and all you should manually do is purchase and use powerups.
 
 ### [Fishtopia](/gamemodes/fishtopia.js)
 
 This should be used in conjunction with the general script.
 
 Automatically answers questions, sells fish without being at the sell station, and allows you to fish without being at water. After using a "travel to..." thing (such as the travel to purple pond boat), you will be able to permanently use it from anywhere.
+
+## Setting up an override
+
+#### Tested browsers that support local overrides
+Chrome and Edge
+
+#### Tested browsers that do not support local overrides
+Firefox
+
+Overrides only work on certain browsers. If the browser you are using supports them, follow the following steps to set the one for  up.
+
+1. Open the console (Ctrl+Shift+I)
+2. Click the "Sources" tab
+3. On the left, open the "Overrides" tab
+4. Hit "Select folder for overrides"
+5. Select a folder to store the override in
+6. If a "www.gimkit.com" subfolder does not exist, create one
+7. Download [App.4382044d.js](/overrides/App.4382044d.js), and place it in the "www.gimkit.com" folder
+
+Whenever you load the page, keep the console open until you see the "Gimkit Cheat Override Loaded" message.
--- gamemodes/fishtopia.js
+++ gamemodes/fishtopia.js
@@ -1,155 +1,219 @@
 (function() {
-	class TodoManager {
-		constructor(todos, hud, toReveal) {
-			this.todos = todos
-			this.hud = hud
-			this.toReveal = toReveal
-
-			let div = document.createElement("div")
-			div.innerHTML = "Please do the following"
-			this.hud.appendChild(div)
-
-			for(let todo of todos) {
-				let div = document.createElement("div")
-				div.innerHTML = todo
-				this.hud.appendChild(div)
-			}
-		}
-		finish(todo) {
-			let index = this.todos.indexOf(todo)
-			if(index == -1) return
-			this.todos.splice(index, 1)
-			let hudElement = Array.from(this.hud.children).find(e => e.innerHTML == todo)
-			hudElement.style.textDecoration = "line-through"
-			hudElement.style.textDecorationThickness = "2.5px"
-
-			if(this.todos.length == 0) {
-				this.hud.remove()
-				if(this.toReveal) this.toReveal.style.display = "block"
-			}
-		}
-	}
-
 	function arrayBufferToString(buffer) {
 		return String.fromCharCode.apply(null, new Uint8Array(buffer));
 	}
 
-	// initialize the cheat hud
-	const injectedCss = new CSSStyleSheet()
-	injectedCss.replaceSync(`
-		.gc_hud {
-			background-color: rgba(0, 0, 0, 0.5) !important;
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 300px;
-			height: 150px;
-			z-index: 999999999;
-			color: white;
-			font-size: 1rem;
-			font-family: Verdana, Geneva, Tahoma, sans-serif;
-			display: flex;
-			flex-direction: column;
-			justify-content: space-around;
-			align-items: center;
-			margin: 1rem;
-			border-radius: 0.5rem;
-		}
+	// initalize standard stuff so multiple scripts can run simultaneously
+	class GCHud {
+		constructor() {
+			this.todos = []
+			
+			this.hud = document.createElement("div")
+			this.hud.classList.add("gc_hud")
+			this.hud.innerHTML = `
+				<div class="gc_todo_msg" style="display:none;">Please do the following:</div>
+			`
+			document.body.appendChild(this.hud)
 
-		.gc_todo {
-			margin: 0;
-			padding: 0;
-			text-align: center;
-		}
-		.gc_hud button {
-			width: 100%;
-			height: 2rem;
-			margin: 0;
-			padding: 0;
-			background-color: rgba(0, 0, 0, 0.5);
-			border: none;
-			border-radius: 0.5rem;
-		}
-		.gc_hud button:hover {
-			border: 1px solid white;
-		}
-		
-		.gc_fasttravel {
-			display: flex;
-			flex-direction: row;
-			justify-content: space-around;
-			align-items: center;
-			width: 100%;
-		}
+			// make the hud draggable
+			let drag = false
+			let dragX = 0
+			let dragY = 0
+			this.hud.addEventListener("mousedown", (e) => {
+				drag = true
+				dragX = e.clientX - this.hud.offsetLeft
+				dragY = e.clientY - this.hud.offsetTop
+			})
+			window.addEventListener("mouseup", () => drag = false)
+			window.addEventListener("mousemove", (e) => {
+				if(drag) {
+					this.hud.style.left = e.clientX - dragX + "px"
+					this.hud.style.top = e.clientY - dragY + "px"
+				}
+			})				
 
-		.gc_fasttravel select {
-			width: 60%;
-			height: 2rem;
-			margin: 0;
-			padding: 0;
-			background-color: rgba(0, 0, 0, 0.5);
-			color: white;
-			font-size: 1rem;
-			font-family: Verdana, Geneva, Tahoma, sans-serif;
-			border: none;
-			border-radius: 0.5rem;
-			margin: 0.35rem;
-		}
-	`)
-	document.adoptedStyleSheets = [injectedCss, ...document.adoptedStyleSheets]
+			// add stylesheets
+			let injectedCss = new CSSStyleSheet()
+			injectedCss.replaceSync(`
+			.gc_hud {
+				background-color: rgba(0, 0, 0, 0.5) !important;
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 300px;
+				height: 150px;
+				z-index: 999999999;
+				color: white;
+				font-size: 1rem;
+				font-family: Verdana, Geneva, Tahoma, sans-serif;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				align-items: center;
+				margin: 1rem;
+				border-radius: 0.5rem;
+			}
 
-	let active = true;
+			.gc_hud button {
+				width: 100%;
+				height: 2rem;
+				margin: 0;
+				padding: 0;
+				background-color: rgba(0, 0, 0, 0.5);
+				border: none;
+				border-radius: 0.5rem;
+			}
+
+			.gc_hud button:hover {
+				border: 1px solid white;
+			}
+
+			.gc_todo .gc_todo_msg {
+				width: 100%;
+			}
+
+			.gc_drop_group {
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				align-items: center;
+				width: 100%;
+			}
+
+			.gc_drop {
+				width: 60%;
+				height: 2rem;
+				margin: 0;
+				padding: 0;
+				background-color: rgba(0, 0, 0, 0.5);
+				color: white;
+				font-size: 1rem;
+				font-family: Verdana, Geneva, Tahoma, sans-serif;
+				border: none;
+				border-radius: 0.5rem;
+				margin: 0.35rem;
+			}
+			`)
+			document.adoptedStyleSheets = [injectedCss]				
+		}
+		addToggleBtn(on, off, callback) {
+			let enabled = false
+			let btn = document.createElement("button")
+			btn.classList.add("gc_toggle")
+			btn.innerHTML = off
+			btn.addEventListener("click", function() {
+				enabled = !enabled
+				this.innerHTML = enabled ? on : off
+				callback(enabled)
+			})
+			btn.addEventListener("keydown", (e) => {
+				e.preventDefault()
+			})
+			this.hud.appendChild(btn)
+		}
+		addTodo(text) {
+			this.todos.push(text)
+			let todo = document.createElement("div")
+			todo.classList.add("gc_todo")
+			todo.innerHTML = text
+			this.hud.querySelector(".gc_todo_msg").after(todo)
+			this.hud.querySelector(".gc_todo_msg").style.display = "block"
+		}
+		completeTodo(text) {
+			if(this.todos.indexOf(text) == -1) return
+			this.hud.querySelectorAll(".gc_todo").forEach((todo) => {
+				if(todo.innerHTML == text) {
+					todo.remove()
+				}
+			})
+			this.todos.splice(this.todos.indexOf(text), 1)
+			if(this.todos.length == 0) this.hud.querySelector(".gc_todo_msg").style.display = "none"
+		}
+		addDropButton(values, callback, btnMsg = "Go") {
+			let group = document.createElement("div")
+			group.classList.add("gc_drop_group")
+			let drop = document.createElement("select")
+			drop.classList.add("gc_drop")
+			values.forEach((value) => {
+				let option = document.createElement("option")
+				option.innerHTML = value
+				drop.appendChild(option)
+			})
+			group.appendChild(drop)
+			let btn = document.createElement("button")
+			btn.classList.add("gc_drop_btn")
+			btn.innerHTML = btnMsg
+			btn.addEventListener("click", () => callback(drop.value))
+			btn.addEventListener("keydown", (e) => {
+				e.preventDefault()
+			})
+			group.appendChild(btn)
+			this.hud.appendChild(group)
+			return {
+				addOption: (value) => {
+					let option = document.createElement("option")
+					option.innerHTML = value
+					drop.appendChild(option)
+				},
+				removeOption: (value) => {
+					drop.querySelectorAll("option").forEach((option) => {
+						if(option.innerHTML == value) option.remove()
+					})
+				}
+			}
+		}
+	}
+
+	if(!window.gcSocket){
+		window.gcSocket = {
+			fake: true,
+			outgoingCallbacks: [],
+			outgoing: function(callback) {
+				if(!this.outgoingCallbacks) this.outgoingCallbacks = []
+				this.outgoingCallbacks.push(callback)
+			}
+		}
+	}
+
+	if(!WebSocket.prototype.outgoing) {
+		WebSocket.prototype._send = WebSocket.prototype.send
+		WebSocket.prototype.send = function(data) {
+			if(window.gcSocket.fake) {
+				this.outgoingCallbacks = window.gcSocket.outgoingCallbacks
+				window.gcSocket = this
+			}
+			if(this.outgoingCallbacks) {
+				for(let callback of this.outgoingCallbacks) {
+					callback(data)
+				}
+			}
+			return this._send(data)
+		}
+		WebSocket.prototype.outgoing = function(callback) {
+			if(!this.outgoingCallbacks) this.outgoingCallbacks = []
+			this.outgoingCallbacks.push(callback)
+		}
+	}
+
+	if(!window.gcHud) {
+		let hud = new GCHud()
+		window.gcHud = hud
+	}
+	window.gcHud.addTodo("Fish a fish")
+	window.gcHud.addTodo("Sell a fish")
+	
+	let autoselling = false;
 	let autofishing = false;
-	let gcHud = document.createElement("div")
-	gcHud.classList.add("gc_hud")
-	gcHud.innerHTML = `
-		<p class="gc_todo"></p>
-		<div class="gc_fasttravel" style="display:none;">
-			<select>
-			</select>
-			<button>Travel</button>
-		</div>
-		<button class="gc_autofish" style="display:none;">Start Autofishing</button>
-		<button class="gc_pause">Pause</button>
-	`
-	document.body.appendChild(gcHud)
-	let todos = new TodoManager(["Answer a question", "Fish a fish", "Sell a fish"],
-	gcHud.querySelector(".gc_todo"), gcHud.querySelector(".gc_autofish"))
-	// prevent the elements from being submitted with enter
-	gcHud.querySelectorAll("button").forEach(e => e.addEventListener('keydown', (e) => {
-		if(e.key == "Enter") e.preventDefault()
-	}))
-
-	gcHud.querySelector(".gc_pause").addEventListener("click", function() {
-		active = !active
-		this.innerHTML = active ? "Pause" : "Resume"
-	})
-	gcHud.querySelector(".gc_autofish").addEventListener("click", function() {
-		autofishing = !autofishing
-		this.innerHTML = autofishing ? "Stop Autofishing" : "Start Autofishing"
-	})
-
-	let socket = null;
 	let lastMenu = null;
-
+	
 	let quickTravel = new Map()
 	let lastTravelPos = null
-
-	gcHud.querySelector(".gc_fasttravel button").addEventListener("click", function() {
-		let select = gcHud.querySelector(".gc_fasttravel select")
-		let pos = quickTravel.get(select.value)
-		if(pos == null) return
-		socket.send(pos)
-	})
-
+	let fastTravelMenu = null
+	
 	let sellMessage = null;
 	let fishMessage = null;
 
-	// override the WebSocket send function to intercept the data
-	let wsSend = WebSocket.prototype.send
-	WebSocket.prototype.send = function(data) {
-		if (socket == null) socket = this
-		wsSend.call(this, data)
+	window.gcSocket.outgoing((data) => {
 		// decode the data from an ArrayBuffer to a string
 		let str = arrayBufferToString(data)
 		if(str.includes("interacted")) {
@@ -158,29 +222,35 @@
 			switch(lastMenu) {
 				case "fishing":
 					console.log("New fish message:", arrayBufferToString(data))
+					if(fishMessage == null) {
+						window.gcHud.completeTodo("Fish a fish")
+						window.gcHud.addToggleBtn("Stop autofishing", "Start autofishing", (enabled) => {
+							autofishing = enabled
+						})
+					}
 					fishMessage = data
-					todos.finish("Fish a fish")
 					break
 				case "selling":
 					console.log("New sell message:", arrayBufferToString(data))
 					sellMessage = data
-					todos.finish("Sell a fish")
+					window.gcHud.completeTodo("Sell a fish")
+					window.gcHud.addToggleBtn("Stop autoselling", "Start autoselling", (enabled) => {
+						autoselling = enabled
+					})
 					break
 				case "travel":
 					if(!lastTravelPos || quickTravel.has(lastTravelPos)) return
 					quickTravel.set(lastTravelPos, data)
 					// update the fast travel menu
-					let select = gcHud.querySelector(".gc_fasttravel select")
-					let option = document.createElement("option")
-					option.value = lastTravelPos
-					option.innerHTML = lastTravelPos
-					select.appendChild(option)
-					let fastTravel = gcHud.querySelector(".gc_fasttravel")
-					fastTravel.style.display = "flex"
-					break
+					if(fastTravelMenu) fastTravelMenu.addOption(lastTravelPos)
+					else {
+						fastTravelMenu = window.gcHud.addDropButton(Array.from(quickTravel.keys()), (pos) => {
+							window.gcSocket._send(quickTravel.get(pos))
+						}, "Travel")
+					}
 			}
 		}
-	}
+	})
 
 	let observer = new MutationObserver(function() {
 		// check if a menu is going to be opened
@@ -216,28 +286,13 @@
 	})
 
 	setInterval(function() {
-		if(!active) return
 		// send a random answer
-		if(sellMessage) socket.send(sellMessage)
+		if(autoselling) window.gcSocket._send(sellMessage)
 		if(autofishing) {
 			// fish for fish
-			if(fishMessage) socket.send(fishMessage)
+			if(fishMessage) window.gcSocket._send(fishMessage)
 		}
 	}, 500)
 
-	let shiftCount = 0
-	let shiftTimeout = null
-	document.addEventListener("keydown", function(e) {
-		if(e.key == "Shift") {
-			shiftCount++
-			if(shiftTimeout) clearTimeout(shiftTimeout)
-			shiftTimeout = setTimeout(function() {
-				shiftCount = 0
-			}, 500)
-			if(shiftCount == 3) {
-				gcHud.style.display = gcHud.style.display == "none" ? "flex" : "none"
-			}
-		}
-	})
 	console.log("Gimkit Cheat Loaded")
 })();
